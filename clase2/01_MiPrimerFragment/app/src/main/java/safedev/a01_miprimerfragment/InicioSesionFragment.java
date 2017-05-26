0848d1a9dee0c0222b535d52f2ca9b59ed305dd5
Germán Maluenda Alfaro <gmaluenda.alfaro@gmail.com>
2017-05-26 00:25:08 -0400

    uso de dos fragments en un activity

---
+++ clase2/01_MiPrimerFragment/app/src/main/java/safedev/a01_miprimerfragment/InicioSesionFragment.java
@@ -0,0 +1,20 @@
+package safedev.a01_miprimerfragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by OvHell on 24-05-2017.
+ */
+
+public class InicioSesionFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_inicio_sesion, container, false);
+    }
+}
---
+++ clase2/01_MiPrimerFragment/app/src/main/java/safedev/a01_miprimerfragment/MainActivity.java
@@ -0,0 +1,13 @@
+package safedev.a01_miprimerfragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
