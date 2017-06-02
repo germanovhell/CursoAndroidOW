5547135b3165600c74b10715b8c00a6037962998
Germán Maluenda Alfaro <gmaluenda.alfaro@gmail.com>
2017-06-02 00:03:35 -0400

    ejemplo basico de uso de GridView con array de strings

---
+++ clase3/02_GridView_simple/app/src/main/java/safedev/a02_gridview_simple/MainActivity.java
@@ -0,0 +1,35 @@
+package safedev.a02_gridview_simple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    String[] personas;
+    GridView cuadricula;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cuadricula = (GridView)findViewById(R.id.gridView);
+
+        personas = new String[] {"Maria","Ana","Camila", "Juana", "Raquel", "Pau"};
+
+        ArrayAdapter<String> adaptadorPersonas = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,personas);
+
+        cuadricula.setAdapter(adaptadorPersonas);
+
+        cuadricula.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Seleccionado : "+personas[position], Toast.LENGTH_SHORT).show();
+                view.animate().setDuration(1000).rotation(360);
+            }
+        });
+    }
+}
